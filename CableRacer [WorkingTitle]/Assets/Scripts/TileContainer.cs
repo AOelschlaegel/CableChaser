ebffd12681acfda9f7001cfc08ec6321a6bc2e80
Albert Oelschlägel <42385563+AOelschlaegel@users.noreply.github.com>
2019-04-08 19:27:04 +0200

    Added SteamVR and Setup Prefabs

---
+++ CableRacer [WorkingTitle]/Assets/Scripts/TileContainer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileContainer : MonoBehaviour
+{
+	[Header("Setup")]
+	[SerializeField] private List<Transform> _lanes = new List<Transform>();
+	[SerializeField] private Material _mainMaterial;
+	[SerializeField] private Transform _endConnector;
+	[SerializeField] private Transform _startConnector;
+
+    void Start()
+    {
+        foreach(var lane in _lanes)
+		{
+			lane.GetComponent<MeshRenderer>().material = _mainMaterial;
+		}
+    }
+}
