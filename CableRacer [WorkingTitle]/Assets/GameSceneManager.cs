fbdbe7731ecff908c38110c006a4484c537ace8d
Albert Oelschlägel <42385563+AOelschlaegel@users.noreply.github.com>
2019-04-10 21:54:37 +0200

    Added UIManager; Added UIPackage

---
+++ CableRacer [WorkingTitle]/Assets/GameSceneManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Collections;
+
+public class GameSceneManager : MonoBehaviour
+{
+	[SerializeField] private string _gameScene;
+	[SerializeField] private string _gameOverScene;
+	[SerializeField] private string _startScene;
+
+
+	public void LoadGameScene()
+	{
+		StartCoroutine(LoadScene(_gameScene));
+	}
+
+	public void LoadGameOverScene()
+	{
+		StartCoroutine(LoadScene(_gameOverScene));
+	}
+
+	public void LoadStartScene()
+	{
+		StartCoroutine(LoadScene(_startScene));
+	}
+
+	public void QuitApplication()
+	{
+		Application.Quit();
+	}
+
+	private IEnumerator LoadScene(string scene)
+	{
+		yield return new WaitForSeconds(2f);
+		SceneManager.LoadScene(scene);
+	}
+}
