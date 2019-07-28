2067fcde2ee974e9d8da59b21fe8afddb769a83d
Albert Oelschlägel <42385563+AOelschlaegel@users.noreply.github.com>
2019-07-28 13:55:21 +0200

    Revert "Worked on Object Pooling for endless mode"
    This reverts commit 59b7aa1c37f0000082b0ff3ce3edecada9a048da.

--- CableRacer [WorkingTitle]/Assets/Scripts/ProceduralGenerator.cs
+++ CableRacer [WorkingTitle]/Assets/Scripts/ProceduralGenerator.cs
@@ -4,227 +4,138 @@ using UnityEngine;
 
 public class ProceduralGenerator : MonoBehaviour
 {
-    [SerializeField] private GameObject _straight;
-    [SerializeField] private GameObject _curve_Right;
-    [SerializeField] private GameObject _curve_Left;
+	[SerializeField] private GameObject _straight;
+	[SerializeField] private GameObject _curve_Right;
+	[SerializeField] private GameObject _curve_Left;
 
-    [SerializeField] private GameObject _obstacle_Left;
-    [SerializeField] private GameObject _obstacle_Right;
-    [SerializeField] private GameObject _obstacle_Straight;
+	[SerializeField] private GameObject _obstacle_Left;
+	[SerializeField] private GameObject _obstacle_Right;
+	[SerializeField] private GameObject _obstacle_Straight;
 
-    [SerializeField] private PlayerController_Endless playerControllerEndless;
+	[SerializeField] private PlayerController_Endless playerControllerEndless;
 
-    private List<GameObject> _obstaclesLeft = new List<GameObject>();
-    private List<GameObject> _obstaclesRight = new List<GameObject>();
-    private List<GameObject> _obstaclesStraight = new List<GameObject>();
+	private List<GameObject> _obstaclesLeft = new List<GameObject>();
+	private List<GameObject> _obstaclesRight = new List<GameObject>();
+	private List<GameObject> _obstaclesStraight = new List<GameObject>();
 
-    public List<GameObject> TilePool;
-    public List<GameObject> SpawnedObstacles = new List<GameObject>();
-    [SerializeField] private Transform _sceneRoot;
+	public List<GameObject> SpawnedTiles;
+	public List<GameObject> SpawnedObstacles = new List<GameObject>();
+	[SerializeField] private Transform _sceneRoot;
 
-    public List<GameObject> StraightTilePool;
-    public List<GameObject> LeftTilePool;
-    public List<GameObject> RightTilePool;
+	private List<GameObject> _tiles;
 
-    public int PoolSize;
+	public int SpawnCount;
+	public int DeleteThreshold;
+	public int CurveSpawnChance;
+	public int StraightSpawnChance;
+	public int LastDeletedIndex = 0;
 
-    private List<GameObject> _tiles;
+	public int ObstacleSpawnDivider;
 
-    public int SpawnCount;
-    public int DeleteThreshold;
-    public int CurveSpawnChance;
-    public int StraightSpawnChance;
-    public int LastDeletedIndex = 0;
-
-    public int SpawnRadius;
-
-    public int ObstacleSpawnDivider;
-
-    public int division = 2;
-
-    public int CurrentTileId;
-
-    [Header("Screen")]
-    public bool isScreen;
+	public int division = 2;
+	
+	[Header("Screen")]
+	public bool isScreen;
 
 
 
-    #region UnityEvents
+	#region UnityEvents
 
-    private void Awake()
-    {
-        _tiles = new List<GameObject>();
-        TilePool = new List<GameObject>();
-        _tiles.Add(_straight);
-        _tiles.Add(_curve_Right);
-        _tiles.Add(_curve_Left);
+	private void Awake()
+	{
+		_tiles = new List<GameObject>();
+		SpawnedTiles = new List<GameObject>();
+		_tiles.Add(_straight);
+		_tiles.Add(_curve_Right);
+		_tiles.Add(_curve_Left);
 
+		var initialTile = _tiles[0];
+		var initialPos = new Vector3(0, 0, 2);
+		var initialInstance = Instantiate(initialTile, initialPos, Quaternion.identity);
+		initialInstance.transform.SetParent(_sceneRoot);
+		SpawnedTiles.Add(initialInstance);
+		initialInstance.name = "tile_0";
+		initialInstance.GetComponent<TileContainer>().Id = 0;
 
+		GenerateTiles();
+	}
 
-        var initialTile = _tiles[0];
-        var initialPos = new Vector3(0, 0, 2);
-        var initialInstance = Instantiate(initialTile, initialPos, Quaternion.identity);
-        initialInstance.transform.SetParent(_sceneRoot);
-        TilePool.Add(initialInstance);
-        initialInstance.name = "tile_0";
-        initialInstance.GetComponent<TileContainer>().Id = 0;
+	void Update()
+	{
+		var tileId = playerControllerEndless.CurrentTileId;
 
-        GenerateTiles();
-
-        InstantiatePool();
-
-    }
-
-    void InstantiatePool()
-    {
-        var poolPos = new Vector3(0, 100000, 0);
-
-        for (int i = 0; i < PoolSize; i++)
-        {
-            var straightInstance = Instantiate(_tiles[0].gameObject, poolPos, Quaternion.identity);
-            StraightTilePool.Add(straightInstance);
-            straightInstance.transform.parent = _sceneRoot;
-
-            var rightInstance = Instantiate(_tiles[1].gameObject, poolPos, Quaternion.identity);
-            RightTilePool.Add(rightInstance);
-            rightInstance.transform.parent = _sceneRoot;
-
-            var leftInstance = Instantiate(_tiles[2].gameObject, poolPos, Quaternion.identity);
-            LeftTilePool.Add(leftInstance);
-            leftInstance.transform.parent = _sceneRoot;
-        }
-    }
-
-    void Update()
-    {
-        var formerTileId = playerControllerEndless.CurrentTileId;
-
-        if (formerTileId > SpawnRadius)
-        {
-            if (formerTileId != CurrentTileId)
-            {
-                RepositionTiles(formerTileId - SpawnRadius, CurrentTileId + 1);
-            }
-        }
-
-        /*if (CurrentTileId > SpawnedTiles.Count - DeleteThreshold)
+		if (tileId > SpawnedTiles.Count - DeleteThreshold)
 		{
 			GenerateTiles();
-			DeactivateTiles(CurrentTileId);
-		}*/
+			DeactivateTiles(tileId);
+		}
+	}
 
-        CurrentTileId = playerControllerEndless.CurrentTileId;
-    }
+	void DeactivateTiles(int tileId)
+	{
+		for (int i = LastDeletedIndex; i < LastDeletedIndex + DeleteThreshold; i++)
+		{
+			SpawnedTiles[i].gameObject.SetActive(false);
+		}
 
-    void RepositionTiles(int formerTileId, int newTileId)
-    {
-        var randTile = Random.Range(0, TilePool.Count);
+		LastDeletedIndex += DeleteThreshold;
+	}
 
-        var repoTile = TilePool[formerTileId - SpawnRadius - 1];
+	#endregion
 
-        repoTile.transform.position = TilePool[TilePool.Count - 1].transform.position;
+	public void GenerateTiles()
+	{
+		for (var i = 0; i < SpawnCount; i++)
+		{
+			var randTile = Random.Range(0, _tiles.Count);
+			var tile = _tiles[randTile];
+			var tileId = tile.GetComponent<TileContainer>().Id;
 
-        //SpawnedTiles.Remove(repoTile);
-        //SpawnedTiles.Add(repoTile);
+			int spawnChance;
+			switch (randTile)
+			{
+				case 0:
+				{
+					spawnChance = Random.Range(5, StraightSpawnChance);
+					SpawnTile(tile, spawnChance);
+					break;
+				}
+				case 1:
+					spawnChance = Random.Range(1, CurveSpawnChance);
+					SpawnTile(tile, spawnChance);
+					break;
+				case 2:
+					spawnChance = Random.Range(1, CurveSpawnChance);
+					SpawnTile(tile, spawnChance);
+					break;
+			}
+		}
+	}
 
-        Debug.Log(repoTile);
-    }
+	
+	public void SpawnObstacles()
+	{
+		if (!isScreen)
+		{
+			var lastTile = SpawnedTiles[SpawnedTiles.Count - 1].gameObject;
+			var instance = Instantiate(_obstacle_Straight, lastTile.transform.position, lastTile.transform.rotation);
+		}
+	}
 
-    void DeactivateTiles(int CurrentTileId)
-    {
-        for (int i = LastDeletedIndex; i < LastDeletedIndex + DeleteThreshold; i++)
-        {
-            TilePool[i].gameObject.SetActive(false);
-        }
-
-        LastDeletedIndex += DeleteThreshold;
-    }
-
-    #endregion
-
-    public void GenerateTiles()
-    {
-        for (var i = 0; i < SpawnCount; i++)
-        {
-            var randTile = Random.Range(0, _tiles.Count);
-            var tile = _tiles[randTile];
-            var tileId = tile.GetComponent<TileContainer>().Id;
-
-            int spawnChance;
-            switch (randTile)
-            {
-                case 0:
-
-                    spawnChance = Random.Range(5, StraightSpawnChance);
-                    SpawnTile(tile, spawnChance);
-                    break;
-                case 1:
-
-                    spawnChance = Random.Range(1, CurveSpawnChance);
-                    SpawnTile(tile, spawnChance);
-                    break;
-                case 2:
-
-                    spawnChance = Random.Range(1, CurveSpawnChance);
-                    SpawnTile(tile, spawnChance);
-                    break;
-            }
-        }
-    }
-
-
-    public void SpawnObstacles()
-    {
-        if (!isScreen)
-        {
-            var lastTile = TilePool[TilePool.Count - 1].gameObject;
-            var instance = Instantiate(_obstacle_Straight, lastTile.transform.position, lastTile.transform.rotation);
-        }
-    }
-
-    private void SpawnTile(GameObject tile, int spawnChance)
-    {
-        for (var i = 0; i < spawnChance; i++)
-        {
-            if (i % ObstacleSpawnDivider == 0 && i != 0)
-            {
-                SpawnObstacles();
-            }
-            var formerTileContainer = TilePool[TilePool.Count - 1].GetComponent<TileContainer>();
-            var newInstance = Instantiate(tile, formerTileContainer.EndConnector.transform.position, formerTileContainer.EndConnector.transform.rotation);
-            newInstance.transform.SetParent(_sceneRoot);
-            newInstance.name = "tile_" + TilePool.Count;
-            newInstance.GetComponent<TileContainer>().Id = TilePool.Count;
-            TilePool.Add(newInstance);
-        }
-    }
-
-    private void SpawnTileFixed(GameObject tile, int spawnChance)
-    {
-        var formerTileContainer = TilePool[TilePool.Count - 1].GetComponent<TileContainer>();
-        var newInstance = Instantiate(tile, formerTileContainer.EndConnector.transform.position, formerTileContainer.EndConnector.transform.rotation);
-        newInstance.transform.SetParent(_sceneRoot);
-        newInstance.name = "tile_" + TilePool.Count;
-        newInstance.GetComponent<TileContainer>().Id = TilePool.Count;
-        TilePool.Add(newInstance);
-    }
-
-
-    /*
-     * var formerTileId = CurrentTileId;
-     * 
-     * 
-     * if(formerTileId != CurrentTileId) {
-     *      RepositionTiles();
-     * 
-     * }
-     * 
-     * 
-     * 
-     * 
-     * 
-     * 
-     * 
-     * */
-
+	private void SpawnTile(GameObject tile, int spawnChance)
+	{
+		for (var i = 0; i < spawnChance; i++)
+		{
+			if (i % ObstacleSpawnDivider == 0 && i != 0)
+			{
+				SpawnObstacles();
+			}
+			var formerTileContainer = SpawnedTiles[SpawnedTiles.Count - 1].GetComponent<TileContainer>();
+			var newInstance = Instantiate(tile, formerTileContainer.EndConnector.transform.position, formerTileContainer.EndConnector.transform.rotation);
+			newInstance.transform.SetParent(_sceneRoot);
+			newInstance.name = "tile_" + SpawnedTiles.Count;
+			newInstance.GetComponent<TileContainer>().Id = SpawnedTiles.Count;
+			SpawnedTiles.Add(newInstance);
+		}
+	}
 }
